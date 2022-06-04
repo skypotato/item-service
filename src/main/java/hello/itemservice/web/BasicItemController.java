09287de531ecd96fa358471bc9f9ca6bf97a52bd
skypotato <hunsooski@naver.com>
2022-06-04 09:07:14 +0900

    2022.06.04 : mvc2 실습 #1

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -37,8 +37,8 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save(
+    // @PostMapping("/add")
+    public String addItemV1(
             @RequestParam String itemName,
             @RequestParam int price,
             @RequestParam Integer quantity,
@@ -49,6 +49,16 @@ public class BasicItemController {
         item.setPrice(price);
         item.setQuantity(quantity);
 
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model
+    ) {
         itemRepository.save(item);
 
         model.addAttribute("item", item);
