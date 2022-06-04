aa6d374d5224afbbfe7edf8294b28f9a5b72153b
skypotato <hunsooski@naver.com>
2022-06-04 09:14:07 +0900

    2022.06.04 : mvc2 실습 #1

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -55,11 +55,19 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item
     ) {
         itemRepository.save(item);
 //        model.addAttribute("item", item); // 자동으로 추가해줌
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item
+    ) {
+        itemRepository.save(item);
 
         return "basic/item";
     }
