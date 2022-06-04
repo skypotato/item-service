9e48efa746560ecf233c7b9b6a41b5dd006dfbd9
skypotato <hunsooski@naver.com>
2022-06-04 09:43:17 +0900

    2022.06.04 : mvc2 실습 #1

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -73,19 +73,17 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String addItemV4( Item item
+    public String addItemV4(Item item
     ) {
         itemRepository.save(item);
 
         return "basic/item";
     }
 
-    @GetMapping("/{itemId}/edit")
-    public String editForm(@PathVariable Long itemId, Model model){
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
-
-        return "basic/editForm";
+    @PostMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
